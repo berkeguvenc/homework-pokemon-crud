cd804c143e42376b7a54533b9cf8668dc81c8830
berkeguvenc <berke.guvenc@outlook.com>
2026-04-25 18:08:48 +0300

    feat: implement axios instance and create usePokemons hook for API integration

---
+++ src/hooks/usePokemons.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../api/axios";
+import type { Pokemon } from "../types/pokemon";
+
+export const usePokemons = () => {
+  return useQuery({
+    queryKey: ["pokemons"],
+    queryFn: () => api.get<Pokemon[]>("/pokemons").then((res) => res.data),
+  });
+};
