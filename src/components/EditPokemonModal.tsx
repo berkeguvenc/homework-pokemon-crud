5b85eac1e51056ecc093a94b9ccc91e90804cc39
berkeguvenc <berke.guvenc@outlook.com>
2026-04-26 21:55:07 +0300

    feat: track form changes in EditPokemonModal to conditionally disable submit button

--- src/components/EditPokemonModal.tsx
+++ src/components/EditPokemonModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonForm from "./PokemonForm";
 import type { Pokemon } from "../types/pokemon";
 
@@ -10,23 +10,41 @@ type Props = {
 };
 
 const EditPokemonModal = ({ isOpen, onClose, pokemon, onSubmit }: Props) => {
+  const [hasChanges, setHasChanges] = useState(false);
+
+  // ASK ?
+  useEffect(() => {
+    setHasChanges(false);
+  }, [pokemon]);
+
   if (!isOpen) return null;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const form = (e.target as HTMLElement).closest("form");
+
+    const formData = new FormData(form);
+    const changed =
+      formData.get("name") !== pokemon.name ||
+      formData.get("type") !== pokemon.type ||
+      formData.get("level") !== String(pokemon.level) ||
+      formData.get("imageUrl") !== pokemon.imageUrl;
+
+    setHasChanges(changed);
+  };
 
   return (
     <dialog className="modal modal-open">
-      <div className="modal-box p-0">
+      <div className="modal-box p-0" onChange={handleChange}>
         {pokemon && (
           <PokemonForm
             isEdit
             initialData={pokemon}
             handleSubmit={onSubmit}
             onCancel={onClose}
+            isDisabled={!hasChanges}
           />
         )}
       </div>
-      <form method="dialog" className="modal-backdrop">
-        <button onClick={onClose}>close</button>
-      </form>
     </dialog>
   );
 };
